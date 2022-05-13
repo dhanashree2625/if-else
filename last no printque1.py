b274e918be208a16410dad37b3ddc57c5ad6c83b
dhanashree2625 <105502568+dhanashree2625@users.noreply.github.com>
2022-05-13 19:28:08 +0530

    Add files via upload

---
+++ last no printque1.py
@@ -0,0 +1,5 @@
+a=int(input("enter the number:"))
+if a%10:
+    print(a%10,"last number")
+else:
+    print("not last digit")
---
+++ yasshreeloop.py
@@ -0,0 +1,60 @@
+# fobanics series
+# n=int(input("enter the number"))
+# x=0
+# y=1
+# z=0
+# while z<=n:
+#     print(z)
+#     x=y
+#     y=z
+#     z=x+y
+
+from re import I
+
+# strong number:
+
+# i=int(input("enter the number"))
+# fac=1
+# while(i>0):
+#     fac=fac*i
+#     i=i-1
+# print("factorial=",fac)
+#  sum=0
+# num=int(input("enter the number:"))
+# temp=num
+# while(num):
+#     i=1
+#     f=1
+#     r=num%10
+#     while(i<=r):
+#         f=f*i
+#         i=i+1
+#     sum=sum+f
+#     num=num//10
+# if(sum==temp):
+#     print("it is a strong number")
+# else:
+#     print("It is not a strong number")
+
+
+# happy number in loop
+
+# num=int(input("enter the number"))
+# temp=num
+# rem=0
+# while sum!=1 and sum!=4:
+#     sum=0
+#     while temp!=0:
+#         rem=int(temp%10)
+#         sum=sum+rem*rem
+#         temp=temp//10
+#     temp=sum
+# if sum==1:
+#     print(num,"it is a happy number")
+#     temp=temp+1
+# else:
+#     print(num,"it is sad number")
+
+
+    
+
